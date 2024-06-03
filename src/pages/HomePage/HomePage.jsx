75b44784b1fce70a7ad034574f87dd8c5764443b
Markéta Musílková <mrs.musilkova@gmail.com>
2024-06-03 21:21:30 +0200

    Loading tasks for plans on the home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,3 +1,5 @@
+const API_URL = import.meta.env.VITE_API_URL ?? '/api';
+
 import { Header } from './Header/Header';
 import './HomePage.css';
 import dayjs from 'dayjs';
@@ -19,15 +21,48 @@ export const HomePage = () => {
   const zbyva = stedryDen.diff(dnes, 'day');
 
   const [plans, setPlans] = useState(null);
+  const [tasks, setTasks] = useState([]);
+  const [planTasks, setPlanTasks] = useState({});
 
   const fetchPlans = async () => {
     const data = await listPlans();
     setPlans(data);
   };
 
+  const fetchTasks = async (plans) => {
+    const allTasks = [];
+
+    for (const plan of plans) {
+      const response = await fetch(`${API_URL}/tasks/${plan.$$id}/`);
+      const data = await response.json();
+      allTasks.push(...data);
+    }
+
+    setTasks(allTasks);
+  };
+
   useEffect(() => {
     fetchPlans();
   }, []);
+
+  useEffect(() => {
+    if (plans) {
+      fetchTasks(plans);
+    }
+  }, [plans]);
+
+  useEffect(() => {
+    if (plans && tasks.length > 0) {
+      const planTasksMap = plans.reduce((acc, plan) => {
+        acc[plan.$$id] = tasks
+          .filter((task) => task.plan === plan.$$id)
+          .map((task) => task.title);
+        return acc;
+      }, {});
+
+      setPlanTasks(planTasksMap);
+    }
+  }, [plans, tasks]);
 
   if (plans === null) {
     return (
@@ -39,6 +74,10 @@ export const HomePage = () => {
       </>
     );
   }
+
+  console.log(plans);
+  console.log(tasks);
+  console.log(planTasks);
 
   return (
     <>
@@ -58,10 +97,11 @@ export const HomePage = () => {
               </AccordionButton>
             </h2>
             <AccordionPanel pb={4}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
+              <ul>
+                {planTasks[plan.$$id]?.map((title, index) => (
+                  <li key={index}>{title}</li>
+                ))}
+              </ul>
             </AccordionPanel>
           </AccordionItem>
         ))}
